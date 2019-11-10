cf7b7d3ba8d9b984076c1268cb4a101d01c568f7
Heethesh Vhavle <heethesh@gmail.com>
2019-11-09 23:25:49 -0500

    Integrated diagnostics

--- src/ego_trajectory_subscriber.py
+++ src/ego_trajectory_subscriber.py
@@ -12,32 +12,49 @@ import math
 
 import rospy
 from nav_msgs.msg import Odometry
+from diagnostic_msgs.msg import DiagnosticArray
 from visualization_msgs.msg import MarkerArray, Marker
 from delta_msgs.msg import EgoStateEstimate, EgoStateEstimateArray
 
+from utils import FPSLogger, make_diagnostics_status
 from ego_trajectory_prediction import EgoTrajectoryPrediction
+
+fps_logger = FPSLogger('Ego State Prediction')
+
+
+def publish_diagnostics(pub):
+    msg = DiagnosticArray()
+    msg.header.stamp = rospy.Time.now()
+    msg.status.append(make_diagnostics_status('ego_vehicle', 'prediction', str(fps_logger.fps)))
+    pub.publish(msg)
 
 
 def main():
-	rospy.init_node('ego_trajectory_prediction', anonymous=True)
-	ego_predictor = EgoTrajectoryPrediction(0.01, 3.0)
-	
-	if rospy.has_param('odom_path'):
-		odom_path = rospy.get_param('odom_path')
-		print('Odometry File:', odom_path)
-		ego_predictor.set_odom_path(odom_path)
+    rospy.init_node('ego_trajectory_prediction', anonymous=True)
+    ego_predictor = EgoTrajectoryPrediction(0.01, 3.0)
 
-	rospy.Subscriber("/carla/ego_vehicle/odometry", Odometry, ego_predictor.callback)
-	pub_vis = rospy.Publisher('/delta/prediction/ego', Marker, queue_size=10)
-	pub_odom = rospy.Publisher('/delta/prediction/ground_truth', Marker, queue_size=10)
-	pub_state = rospy.Publisher('/delta/prediction/ego_vehicle/state', EgoStateEstimate, queue_size=10)
-	pub_traj = rospy.Publisher('/delta/prediction/ego_vehicle/trajectory', EgoStateEstimateArray, queue_size=10)
-	r = rospy.Rate(10)
+    if rospy.has_param('odom_path'):
+        odom_path = rospy.get_param('odom_path')
+        print('Odometry File:', odom_path)
+        ego_predictor.set_odom_path(odom_path)
 
-	while not rospy.is_shutdown():
-		ego_predictor.run(pub_vis, pub_odom, pub_state, pub_traj)
-		r.sleep()
+    rospy.Subscriber("/carla/ego_vehicle/odometry", Odometry, ego_predictor.callback)
+    pub_vis = rospy.Publisher('/delta/prediction/ego', Marker, queue_size=10)
+    pub_odom = rospy.Publisher('/delta/prediction/ground_truth', Marker, queue_size=10)
+    pub_state = rospy.Publisher('/delta/prediction/ego_vehicle/state', EgoStateEstimate, queue_size=10)
+    pub_traj = rospy.Publisher('/delta/prediction/ego_vehicle/trajectory', EgoStateEstimateArray, queue_size=10)
+    pub_diag = rospy.Publisher('/delta/prediction/ego_vehicle/diagnostics', DiagnosticArray, queue_size=5)
+
+    r = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        fps_logger.lap()
+        ego_predictor.run(pub_vis, pub_odom, pub_state, pub_traj)
+        fps_logger.tick()
+
+        publish_diagnostics(pub_diag)
+        r.sleep()
 
 
 if __name__ == '__main__':
-	main()
+    main()
